33de0eefc9f1f855f5eea50ccb5df5d969225643
brunaclimaco <bvictoriaclimaco@gmail.com>
2022-02-16 18:56:35 -0300

    Elaboração do código "codigo_v01.R" para a 2° pergunta do subtema "Cinema"

---
+++ codigo_v01.R
@@ -0,0 +1,134 @@
+######################################################
+# 1) Carregar bibliotecas
+
+library(tidyverse)
+library(magrittr)
+#library(dplyr)
+library(readr)
+library(rjson)
+library(RJSONIO)
+
+# # Library para importar dados SQL
+# library(DBI)
+# library(RMySQL)
+# library(pool)
+# library(sqldf)
+# library(RMariaDB)
+# 
+# # Carragamento de banco de dados
+# 
+# # Settings
+# db_user <-'admin'
+# db_password <-'password'
+# db_name <-'cdnaep'
+# #db_table <- 'your_data_table'
+# db_host <-'127.0.0.1' # for local access
+# db_port <-3306
+# 
+# # 3. Read data from db
+# # drv=RMariaDB::MariaDB(),
+# mydb <-  dbConnect(drv =RMariaDB::MariaDB(),user =db_user, 
+#                    password = db_password ,
+#                    dbname = 'cdnaep', host = db_host, port = db_port)
+# 
+# dbListTables(mydb)
+# 
+# s <- paste0("SELECT * from", " consumo_agua")
+# rs<-NULL
+# rs <- dbSendQuery(mydb, s)
+# 
+# dados<- NULL
+# dados <-  dbFetch(rs, n = -1)
+# dados
+# #dbHasCompleted(rs)
+# #dbClearResult(rs)
+
+library(readr)
+cine <- read_csv("data/cinema_ssa_ba - cinema.csv")
+View(cine)
+
+dados2 <- cine %>% select(ano,quant_complexos_ssa)
+
+##  Perguntas e titulos 
+T_ST_P_No_Culturaesporte <- read_csv("data/TEMA_SUBTEMA_P_No - CULTURAESPORTE.csv")
+dados2 %<>% gather(key = classe,
+                  value = quant_complexos_ssa,-ano) 
+#dados %<>% select(-id)
+# Temas Subtemas Perguntas
+
+
+
+## Arquivo de saida 
+
+SAIDA_POVOAMENTO <- T_ST_P_No_Culturaesporte %>% 
+  select(TEMA,SUBTEMA,PERGUNTA,NOME_ARQUIVO_JS)
+SAIDA_POVOAMENTO <- as.data.frame(SAIDA_POVOAMENTO)
+
+classes <- NULL
+classes <- levels(as.factor(dados2$classe))
+
+# Cores secundarias paleta pantone -
+corsec_recossa_azul <- c('#175676','#62acd1','#8bc6d2','#20cfef',
+                         '#d62839','#20cfef','#fe4641','#175676',
+                         '#175676','#62acd1','#8bc6d2','#20cfef')
+
+#for ( i in 1:length(classes)) {
+
+objeto_0 <- dados2 %>%
+  filter(classe %in% c(classes[1])) %>%
+  select(ano,quant_complexos_ssa) %>% #filter(ano<2019) %>%
+  #arrange(trimestre) %>%
+  mutate(ano = as.character(ano)) %>% list()               
+
+exportJson0 <- toJSON(objeto_0)
+
+
+titulo<-T_ST_P_No_Culturaesporte$TITULO[2]
+subtexto<-"Fonte: OCA/ANCINE"
+link <- T_ST_P_No_Culturaesporte$LINK[2]
+
+data_axis <- paste('["',gsub(' ','","',
+                             paste(paste(as.vector(objeto_0[[1]]$ano)),
+                                   collapse = ' ')),'"]',sep = '')
+
+
+data_serie <- paste('[',gsub(' ',',',
+                             paste(paste(as.vector(objeto_0[[1]]$quant_complexos_ssa)),
+                                   collapse = ' ')),']',sep = '')
+
+texto<-paste('{"title":{"text":"',titulo,
+             '","subtext":"',subtexto,
+             '","sublink":"',link,'"},',
+             '"tooltip":{"trigger":"axis"},',
+             '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":45,"show":true,',
+             '"feature":{"dataZoom":{"yAxisIndex":"none"},',
+             '"dataView":{"readOnly":false},',
+             '"restore":{},"saveAsImage":{}}},"xAxis":{"type":"category",',
+             '"data":',data_axis,'},',
+             '"yAxis":{"type":"value","axisLabel":{"formatter":" {value}"}},',
+             '"series":[{"data":',data_serie,',',
+             '"type":"bar","color":"',corsec_recossa_azul[1],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},',
+             '"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[1],'","borderWidth":2}}]}',sep='')
+
+#SAIDA_POVOAMENTO$CODIGO[i] <- texto   
+texto<-noquote(texto)
+
+
+write(exportJson0,file = paste('data/',gsub('.csv','',T_ST_P_No_Culturaesporte$NOME_ARQUIVO_JS[2]),
+                               '.json',sep =''))
+write(texto,file = paste('data/',T_ST_P_No_Culturaesporte$NOME_ARQUIVO_JS[2],
+                         sep =''))
+
+#}
+
+# Arquivo dedicado a rotina de atualizacao global. 
+
+write_csv2(SAIDA_POVOAMENTO,file ='data/POVOAMENTO.csv',quote='all',escape='none')
+#quote="needed")#,escape='none')
+
+
+objeto_autm <- SAIDA_POVOAMENTO %>% list()
+exportJson_aut <- toJSON(objeto_autm)
+
+#write(exportJson_aut,file = paste('data/povoamento.json'))
